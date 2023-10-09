aea2e623028e63748f4c322d4b2d887b3e931b95
mrela11 <nizam@medzhidov.ru>
2023-10-09 20:19:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,323 @@
+import logo from './media_files/icon/Union (1).png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main_container'>
+        <div className='header_container'>
+          <a className='logo_header'>
+            <img alt="logo" src={logo}/>
+          </a>
+          <div className='bar_container'>
+            <div className='bar_item'>DISCOVER</div>
+            <div className='bar_item'>CREATORS</div>
+            <div className='bar_item'>SELL</div>
+            <div className='bar_item'>STATS</div>
+          </div>
+          <input className='search_input' type='text' placeholder='Search Art Work / Creator' />
+          <button className='button_connect'><div className='div_button_connect'>CONNECT WALLET</div></button>
+        </div>
+        <div className='section_ifno'>
+          <div className='wrapper'>
+            <div className='hero_text'>
+              <div className='line_top'></div>
+              <div className='hero_content_h1'>
+                <h1 className='hero_h1'>Discover And Create NFTs</h1>
+              </div>
+              <div className='hero_content_h2'>
+                <h2 className='hero_h1'>Discover, Create and Sell NFTs On Our NFT Marketplace With Over Thousands Of NFTs And Get a $20 bonus.</h2>
+              </div>
+              <div className='hero_button'>
+                <button><div>EXPLORER MORE</div></button>
+                <button><div>CREATE NFT</div></button>
+              </div>
+            </div>
+            <div className='featurs'>
+              <div className='list'>
+                <div className='list_1'>
+                  <div className='list_1_num'>430+</div>
+                  <div className='list_1_title'>Art works</div>
+                </div>
+                <div className='list_2'>
+                  <div className='list_2_num'>159+</div>
+                  <div className='list_2_title'>Creators</div>
+                </div>
+                <div className='list_3'>
+                  <div className='list_3_num'>87+</div>
+                  <div className='list_3_title'>Collections</div>
+                </div>
+              </div>
+            </div>
+            <div className='hero_header'>
+              <div className='hero_header_img_1'>
+                <img className='hero_header_img_1!' src='#' />
+              </div>
+              <div className='hero_header_arrow'>
+                <img className='hero_header_arrow!' alt='logo' src='#' />
+              </div>
+              <div className='hero_header_img_2'>
+                <img className='hero_header_img_2!' alt='logo' src='#' />
+              </div>
+              <div className='hero_header_img_3'>
+                <img className='hero_header_img_3!' alt='logo' src='#' />
+              </div>
+              <div className='nagigator'>
+                <button></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='section_weekly'>
+          <h1>Weekly - Top NFT</h1>
+          <div className='trending'>
+            <div className='trending_items'>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='trending_item_container'>
+                <div className='trending_item'>
+                  <div className='trading_item_img_cont'>
+                    <img className='trading_item_img' alt='logo' src='#' />
+                    <div className='trending_item_time'><div>07h 09m 12s</div></div>
+                  </div>
+                  <div className='trading_item_inf_cont'>
+                    <div className='trading_item_title'>Sun-Glass</div>
+                    <div className='trading_item_inf!_cont'>
+                      <div className='current_inf'>
+                        <div>Current bid</div>
+                        <div className='current_inf!'>
+                          <div className='current_inf!_icon'>
+                            <img className='current_inf!_icon_logo' alt='logo' src='#' />
+                          </div>
+                          <div className='current_inf!_num'>
+                            <div className='current_inf!_num!'>1/75</div>
+                          </div>
+                        </div>
+                      </div>
+                      <button><div>PLACE BID</div></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='current_nagigator'>
+                <button className='current_nagigator_arrow'></button>
+              </div>
+              <div className='recently_viewd_container'>
+                <div className='recently_viewd'>
+                  <div className='recently_wallet'>
+                    <div className='recently_wallet_title'>Recent Viewed</div>
+                    <div className='recently_other'>
+                      <img className='recently_other_img' alt='logo' src='#' />
+                    </div>
+                  </div>
+                  <div className='item'></div>
+                  <div className='item'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default App;
